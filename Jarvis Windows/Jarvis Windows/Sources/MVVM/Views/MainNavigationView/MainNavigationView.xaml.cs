4eded8002d4cd0a47ea8972e392a1dc940dec862
TranTrongDai <trongdaitran0903@gmail.com>
2024-05-23 03:20:48 +0700

    chore: move init tray icon into main navigation page

--- Jarvis Windows/Jarvis Windows/Sources/MVVM/Views/MainNavigationView/MainNavigationView.xaml.cs
+++ Jarvis Windows/Jarvis Windows/Sources/MVVM/Views/MainNavigationView/MainNavigationView.xaml.cs
@@ -1,14 +1,20 @@
 ﻿using Gma.System.MouseKeyHook;
 using Jarvis_Windows.Sources.MVVM.Models;
 using Jarvis_Windows.Sources.MVVM.Views.AIChatBubbleView;
+using Jarvis_Windows.Sources.MVVM.Views.ContextMenuView;
 using Jarvis_Windows.Sources.MVVM.Views.MenuInjectionActionsView;
 using Jarvis_Windows.Sources.Utils.Services;
 using System;
 using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Forms;
 using System.Windows.Input;
+
+using Point = System.Windows.Point;
+
 
 namespace Jarvis_Windows.Sources.MVVM.Views.MainNavigationView
 {
@@ -16,9 +22,11 @@ namespace Jarvis_Windows.Sources.MVVM.Views.MainNavigationView
     {
         private bool _isBubbleClicked = false;
         private Point _bubbleClickedPosition;
+        private NotifyIcon _notifyIcon;
         public MainNavigationView()
         {
             InitializeComponent();
+            InitTrayIcon();
 
             this.Width = 474 + 70;
             this.Height = SystemParameters.WorkArea.Height * 0.98;
@@ -32,6 +40,26 @@ namespace Jarvis_Windows.Sources.MVVM.Views.MainNavigationView
             aIChatBubblePopup.VerticalOffset = (int)((SystemParameters.WorkArea.Height - /*aIChatBubblePopup.Height*/ 260) / 2);
 
             EventAggregator.PropertyMessageChanged += MainNavigationView_Activated;
+        }
+
+        private void InitTrayIcon()
+        {
+            string relativePath = Path.Combine("Assets", "Icons", "jarvis_logo_large.ico");
+            string fullPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath));
+            _notifyIcon = new NotifyIcon();
+            _notifyIcon.Icon = new Icon(fullPath);
+            _notifyIcon.MouseClick += NotifyIcon_MouseClick;
+            _notifyIcon.Visible = true;
+            _notifyIcon.ContextMenuStrip = new CustomContextMenuView();
+        }
+
+        private void NotifyIcon_MouseClick(object? sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                EventAggregator.PublishPropertyMessageChanged(new PropertyMessage("IsShowMainNavigation", true), new EventArgs());
+                _ = GoogleAnalyticService.Instance().SendEvent("open_main_window");
+            }
         }
 
         private void MouseDown(object sender, System.Windows.Input.MouseEventArgs e)
